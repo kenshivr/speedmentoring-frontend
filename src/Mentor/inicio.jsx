29e141eadc54e5f52a474fe92afcd5c9a75844c4
Erick Pérez Mendoza <erickpm.8d@gmail.com>
2024-08-10 18:03:08 -0600

    Se solucionó el error de inicio de sesioón del mentor.

--- src/Mentor/inicio.jsx
+++ src/Mentor/inicio.jsx
@@ -13,22 +13,35 @@ const MentorPage = () => {
     fetch(`http://localhost:3001/api/showSesionesMentor/${userId}`)
       .then(response => response.json())
       .then(data => {
-        setSessions(data.data);
-        setFilteredSessions(data.data);
+        if (Array.isArray(data.data)) {
+          setSessions(data.data);
+          setFilteredSessions(data.data);
+        } else {
+          console.error('Data is not an array:', data.data);
+        }
       })
       .catch(error => console.error('Error fetching sessions:', error));
   }, [userId]);
+  
 
   useEffect(() => {
     // Filtrar sesiones basadas en la búsqueda
-    setFilteredSessions(
-      sessions.filter(session => 
-        session.SesionID.toString().includes(search) ||
-        session.Fecha.includes(search) ||
-        session.Titulo.includes(search)
-      )
-    );
+    if (sessions) {
+      setFilteredSessions(
+        sessions.filter(session => 
+          session.SesionID.toString().includes(search) ||
+          session.Fecha.includes(search) ||
+          session.Titulo.includes(search)
+        )
+      );
+    }
   }, [search, sessions]);
+
+  useEffect(() => {
+    console.log('Sessions:', sessions);
+    console.log('Filtered Sessions:', filteredSessions);
+  }, [sessions, filteredSessions]);
+  
 
   function handleLink(session) {
     return () => {
