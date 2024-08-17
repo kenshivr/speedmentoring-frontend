b885a0d6d746dc83468402f99220f9cdcfd11991
Erick Pérez Mendoza <erickpm.8d@gmail.com>
2024-08-17 16:47:31 -0600

    Se actualizaron los estilos de los botones de reportes

--- src/Estudiante/sesiones.jsx
+++ src/Estudiante/sesiones.jsx
@@ -113,14 +113,14 @@ export default function Page({ userId }) {
                           to='/Estudiante/sesiones/verSesion'
                           onClick={handleCLickLinkSesion(session.SesionID)}
                         >
-                          <span>✔</span>
+                          <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#48dd48"><path d="m429-336 238-237-51-51-187 186-85-84-51 51 136 135Zm51 240q-79 0-149-30t-122.5-82.5Q156-261 126-331T96-480q0-80 30-149.5t82.5-122Q261-804 331-834t149-30q80 0 149.5 30t122 82.5Q804-699 834-629.5T864-480q0 79-30 149t-82.5 122.5Q699-156 629.5-126T480-96Zm0-72q130 0 221-91t91-221q0-130-91-221t-221-91q-130 0-221 91t-91 221q0 130 91 221t221 91Zm0-312Z"/></svg>
                         </Link>
                       ) : (
                         <Link 
                           to='/Estudiante/sesiones/verSesion'
                           onClick={handleCLickLinkSesion(session.SesionID)}
                         >
-                          n/a
+                          <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#ff0000"><path d="M480-96q-16 0-28-5t-23-16L117-429q-11-11-16-23t-5-28q0-16 5-28t15.7-22.7L429-843q11-11 23-16t28-5q16 0 28 5t23 16l312 312q11 11 16 23t5 28q0 16-5 28t-16 23L530.7-116.7Q520-106 508-101t-28 5Zm0-72 312-312-312-312-312 312 312 312Zm-36-288h72v-216h-72v216Zm35.79 120q15.21 0 25.71-10.29t10.5-25.5q0-15.21-10.29-25.71t-25.5-10.5q-15.21 0-25.71 10.29t-10.5 25.5q0 15.21 10.29 25.71t25.5 10.5Zm.21-144Z"/></svg>
                         </Link>
                       )}
                     </td>
