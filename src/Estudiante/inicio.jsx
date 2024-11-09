a576f2f17f42f5935ad89c23a5a2f958ffc200e2
kenshivr <Aaladar@proton.me>
2024-11-09 17:36:59 -0600

    Se agrega la eliminacion de sesiones y se muestra fecha y hora en sesiones

--- src/Estudiante/inicio.jsx
+++ src/Estudiante/inicio.jsx
@@ -118,14 +118,17 @@ const StudentPage = () => {
   }
 
   const formatDate = (dateString) => {
-    const [year, month, day] = dateString.split('T')[0].split('-');
-    
-    // Asegura que día y mes tengan siempre dos dígitos
-    const formattedDay = day.padStart(2, '0');
-    const formattedMonth = month.padStart(2, '0');
+    if (!dateString) return '';
+
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
   
-    return `${formattedDay}-${formattedMonth}-${year}`;
-  };
+    return `${day}/${month}/${year} - ${hours}:${minutes}`;
+  };  
 
   return (
     <div className='container p-5'>
--- src/Mentor/inicio.jsx
+++ src/Mentor/inicio.jsx
@@ -1,18 +1,46 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-
-import LinkPrincipalCentered from './../components/Link/LinkPrincipalCentered.jsx';
+import DropButton2 from '../components/Button/DropButton2.jsx';
 import SearchBarNoButton from '../components/Search/SearchBarNoButton.jsx';
-import DropButton1 from '../components/Button/DropButton1.jsx';
+import LinkPrincipalCentered from './../components/Link/LinkPrincipalCentered.jsx';
 
 const MentorPage = () => {
+  const [search, setSearch] = useState('');
   const [sessions, setSessions] = useState([]);
   const [filteredSessions, setFilteredSessions] = useState([]);
-  const [search, setSearch] = useState('');
 
   const userId = sessionStorage.getItem('userId');
-
-useEffect(() => {
   const apiUrl = process.env.REACT_APP_BACKEND_URL;
+
+  useEffect(() => {
+    fetchSessions();
+  }, [userId]);
+
+  useEffect(() => {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0); // Elimina la hora para comparar solo la fecha
+
+    const filtered = sessions.filter(session => {
+      // Convierte la fecha de la sesión en un objeto Date
+      const sessionDate = new Date(session.Fecha);
+      sessionDate.setHours(0, 0, 0, 0); // Elimina la hora para comparar solo la fecha
+
+      // Aplica ambos filtros: búsqueda y fecha posterior a hoy
+      return (
+        // Búsqueda en múltiples campos
+        ((session.Titulo ? session.Titulo.toString().toLowerCase() : '').includes(search.toLowerCase()) ||
+          (session.NumeroDeSesion ? session.NumeroDeSesion.toString() : '').includes(search) ||
+          (session.Fecha ? session.Fecha.toString() : '').includes(search) ||
+          (session.Nombre ? session.Nombre.toString().toLowerCase() : '').includes(search.toLowerCase()) ||
+          (session.ApellidoMaterno ? session.ApellidoMaterno.toString().toLowerCase() : '').includes(search.toLowerCase()) ||
+          (session.ApellidoPaterno ? session.ApellidoPaterno.toString().toLowerCase() : '').includes(search.toLowerCase())) &&
+        // Filtrar por fecha
+        sessionDate >= today
+      );
+    });
+
+    setFilteredSessions(filtered);
+  }, [search, sessions]);
 
   // Obtener sesiones desde el servidor solo una vez al montar el componente
   const fetchSessions = async () => {
@@ -28,52 +56,39 @@ useEffect(() => {
     }
   };
 
-  fetchSessions();
-}, [userId]);
-
-useEffect(() => {
-  const today = new Date();
-  today.setHours(0, 0, 0, 0); // Elimina la hora para comparar solo la fecha
-
-  const filtered = sessions.filter(session => {
-    // Convierte la fecha de la sesión en un objeto Date
-    const sessionDate = new Date(session.Fecha);
-    sessionDate.setHours(0, 0, 0, 0); // Elimina la hora para comparar solo la fecha
-
-    // Aplica ambos filtros: búsqueda y fecha posterior a hoy
-    return (
-      // Búsqueda en múltiples campos
-      ((session.Titulo ? session.Titulo.toString().toLowerCase() : '').includes(search.toLowerCase()) ||
-      (session.NumeroDeSesion ? session.NumeroDeSesion.toString() : '').includes(search) ||
-      (session.Fecha ? session.Fecha.toString() : '').includes(search) ||
-      (session.Nombre ? session.Nombre.toString().toLowerCase() : '').includes(search.toLowerCase()) ||
-      (session.ApellidoMaterno ? session.ApellidoMaterno.toString().toLowerCase() : '').includes(search.toLowerCase()) ||
-      (session.ApellidoPaterno ? session.ApellidoPaterno.toString().toLowerCase() : '').includes(search.toLowerCase())) &&
-      // Filtrar por fecha
-      sessionDate >= today
-    );
-  });
-
-  setFilteredSessions(filtered);
-}, [search, sessions]);
-
-
   function handleLink(session) {
     return () => {
       sessionStorage.setItem('sesionId', session.SesionID);
     };
   }
 
-  const formatDate = (dateString) => {
-    const [year, month, day] = dateString.split('T')[0].split('-');
-    
-    // Asegura que día y mes tengan siempre dos dígitos
-    const formattedDay = day.padStart(2, '0');
-    const formattedMonth = month.padStart(2, '0');
-  
-    return `${formattedDay}-${formattedMonth}-${year}`;
+  const handleDelete = async (sesionID) => {
+    try {
+      const response = await axios.delete(`${apiUrl}/api/deleteSession/${sesionID}`);
+      if (response.data.message) {
+        alert(response.data.message);
+      } else {
+        alert('Ocurrio un error en la eliminacion de la sesion');
+      }
+    } catch (error) {
+      console.error('Error al eliminar la sesion:', error);
+    }
+
+    fetchSessions();
   };
+
+  const formatDate = (dateString) => {
+    if (!dateString) return '';
+
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
   
+    return `${day}/${month}/${year} - ${hours}:${minutes}`;
+  };  
 
   const handleSearchChange = (e) => {
     setSearch(e.target.value);
@@ -87,11 +102,11 @@ useEffect(() => {
             className="text-uppercase font-weight-bold"
             style={{
               fontSize: 'clamp(2rem, 5vw, 3rem)',
-              color: 'white', 
+              color: 'white',
               letterSpacing: '2px'
             }}
           >
-           Agenda
+            Agenda
           </p>
         </header>
       </div>
@@ -122,10 +137,12 @@ useEffect(() => {
                     <td>{session.Titulo ? session.Titulo : ''}</td>
                     <td>{`${session.Nombre} ${session.ApellidoPaterno} ${session.ApellidoMaterno}`}</td>
                     <td>
-                      <DropButton1
-                        text='Editar'
-                        link='/Mentor/sesiones/editarSesion'
-                        dropOnClick={handleLink(session)}
+                      <DropButton2
+                        text1='Editar'
+                        link1='/Mentor/sesiones/editarSesion'
+                        dropOnClick1={handleLink(session)}
+                        text2='Eliminar'
+                        dropOnClick2={() => handleDelete(session.SesionID)}
                       />
                     </td>
                   </tr>
